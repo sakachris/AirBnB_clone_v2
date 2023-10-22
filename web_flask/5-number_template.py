57e508aba62c46f5b69ca7f8c2c3fafeda8d6342
sakachris <sakachris90@gmail.com>
2023-10-23 01:53:29 +0300

    hbnb

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -61,4 +61,4 @@ def number_template(n):
 
 if __name__ == "__main__":
     """listening on 0.0.0.0, port 5000"""
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
