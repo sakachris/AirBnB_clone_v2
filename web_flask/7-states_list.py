f00974d81435e07909cecc93bb546e0cb56090be
sakachris <sakachris90@gmail.com>
2023-10-22 21:51:30 +0300

    state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -29,4 +29,4 @@ def states_list():
 
 if __name__ == "__main__":
     """listening on 0.0.0.0, port 5000"""
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
