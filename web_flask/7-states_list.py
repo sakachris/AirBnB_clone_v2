c1bea3f6a447f136d1b7155412274085dbfaa642
sakachris <sakachris90@gmail.com>
2023-10-22 07:07:37 +0300

    states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+    to remove the current SQLAlchemy Session after each request
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    display a HTML page: (inside the tag BODY)
+    """
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    """listening on 0.0.0.0, port 5000"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
