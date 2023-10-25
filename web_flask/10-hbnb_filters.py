2e159779e01c58e94ba6c0e88b4a2032fab5c573
sakachris <sakachris90@gmail.com>
2023-10-25 23:29:21 +0300

    hbnb

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -32,4 +32,4 @@ def hbnb_filters():
 
 if __name__ == "__main__":
     """listening on 0.0.0.0, port 5000"""
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
